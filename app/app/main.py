69d87f47bad40f4c89b18305c03c5c8f38450712
Marcus Jörgensson <marcusjorgensson@gmail.com>
2021-07-10 19:30:04 +0200

    Add endpoint to update counter

--- app/app/main.py
+++ app/app/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 @app.get("/counter")
 def read_counter():
+    # Todo: get counter.
     return 0
+
+@app.put("/counter")
+def update_counter(counter: int = Body(..., embed=True)):
+    # Todo: set counter.
+    return counter
+
