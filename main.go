41fa1e641b23ebe7ce3772c98da5920c1d59921c
Marcus Jörgensson <marcusjorgensson@gmail.com>
2023-01-07 21:37:16 +0100

    Add counter logic

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt";
+import (
+  "fmt"
+  "net/http"
+  "log"
+);
+
+var count = 0;
 
 func main() {
-  fmt.Println("Hello world");
+  log.Println("Starting counter!");
+
+  http.HandleFunc("/", showCountHandler);
+  http.HandleFunc("/increment", incrementHandler);
+  http.HandleFunc("/decrement", decrementHandler);
+
+  log.Fatal(http.ListenAndServe(":80", nil));
+
 }
+
+func showCountHandler(w http.ResponseWriter, req *http.Request) {
+  fmt.Fprintf(w, "%d", count);
+}
+
+func incrementHandler(w http.ResponseWriter, req *http.Request) {
+  count += 1;
+  fmt.Fprintf(w, "Ok");
+}
+
+func decrementHandler(w http.ResponseWriter, req *http.Request) {
+  count -= 1;
+  fmt.Fprintf(w, "Ok");
+}
+
